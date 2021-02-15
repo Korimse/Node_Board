a4e3edc41c57d92a4e248129e84647f2d6b8826a
korimse <korimse@gmail.com>
2021-02-16 00:18:06 +0900

    접근제한

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
+var util = require('../util');
 
-router.get('/', (req, res) => {
-    User.find({})
-    .sort({username:1})
-    .exec((err, users)=>{
-        if(err) return res.json(err);
-        res.render('users/index', {users:users});
-    });
-});
+const checkPermission = (req, res, next) => {
+  User.findOne({username:req.params.username}, function(err, user){
+   if(err) return res.json(err);
+   if(user.id != req.user.id) return util.noPermission(req, res);
+ 
+   next();
+  });
+ }
 
 router.get('/new', (req, res)=>{
     var user = req.flash('user')[0] || {};
@@ -21,25 +22,25 @@ router.post('/', (req, res) => {
     User.create(req.body, (err, user) => {
         if(err) {
             req.flash('user', req.body);
-            req.flash('errors', parseError(err));
+            req.flash('errors', util.parseError(err));
             return res.redirect('/users/new');
         }
-        res.redirect('/users');
+        res.redirect('/');
     });
 });
 
-router.get('/:username', (req, res) => {
+router.get('/:username', util.isLoggedin, (req, res) => {
     User.findOne({username:req.params.username}, (err, user) => {
         if(err) return res.json(err);
         res.render('users/show', {user:user});
     });
 });
 
-router.get('/:username/edit', function(req, res){
+router.get('/:username/edit', util.isLoggedin, checkPermission, (req, res) => {
     var user = req.flash('user')[0];
     var errors = req.flash('errors')[0] || {};
     if(!user){
-      User.findOne({username:req.params.username}, function(err, user){
+      User.findOne({username:req.params.username}, (err, user) => {
         if(err) return res.json(err);
         res.render('users/edit', { username:req.params.username, user:user, errors:errors });
       });
@@ -49,51 +50,27 @@ router.get('/:username/edit', function(req, res){
     }
   });
 
-  router.put('/:username', function(req, res, next){
-    User.findOne({username:req.params.username})
-      .select('password')
-      .exec(function(err, user){
-        if(err) return res.json(err);
-  
-        user.originalPassword = user.password;
-        user.password = req.body.newPassword? req.body.newPassword : user.password;
-        for(var p in req.body){
-          user[p] = req.body[p];
-        }
-  
-        user.save(function(err, user){
-          if(err){
-            req.flash('user', req.body);
-            req.flash('errors', parseError(err));
-            return res.redirect('/users/'+req.params.username+'/edit');
-          }
-          res.redirect('/users/'+user.username);
-        });
-    });
-  });
+router.put('/:username', util.isLoggedin, checkPermission, (req, res, next) => {
+  User.findOne({username:req.params.username})
+    .select('password')
+    .exec((err, user) => {
+      if(err) return res.json(err);
 
-router.delete('/:username', (req, res) => {
-    User.deleteOne({username:req.params.username}, (err) => {
-        if (err) return res.json(err);
-        res.redirect('/users');
-    });
+      user.originalPassword = user.password;
+      user.password = req.body.newPassword? req.body.newPassword : user.password;
+      for(var p in req.body){
+        user[p] = req.body[p];
+      }
+  
+      user.save((err, user) => {
+        if(err){
+          req.flash('user', req.body);
+          req.flash('errors', util.parseError(err));
+          return res.redirect('/users/'+req.params.username+'/edit');
+        }
+        res.redirect('/users/'+user.username);
+      });
+  });
 });
 
 module.exports = router;
-
-var parseError = (errors) => {
-    var parsed = {};
-    if(errors.name == 'ValidationError'){
-        for(var name in errors.errors){
-            var ValidationError = errors.errors[name];
-            parsed[name] = {message:ValidationError.message};
-        }
-    }
-    else if(errors.code == '11000' && errors.errmsg.indexOf('username') > 0){
-        parsed.username = {message:'This username already exists!'};
-    }
-    else{
-        parsed.unhandled = JSON.stringify(errors);
-    }
-    return parsed;
-}
