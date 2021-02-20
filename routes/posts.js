79e474b38146246954ce935c72eaa2a97a07f032
korimse <korimse@gmail.com>
2021-02-20 23:19:39 +0900

    search

--- routes/posts.js
+++ routes/posts.js
@@ -12,6 +12,22 @@ const checkPermission = (req, res, next) => {
     });
 }
 
+const createSearchQuery = (queries) => {
+    var searchQuery = {};
+    if (queries.searchType && queries.searchText && queries.searchText.length >= 3){
+        var searchTypes = queries.searchType.toLowerCase().split(',');
+        var postQueries = [];
+        if (searchTypes.indexOf('title')>=0){
+            postQueries.push({title:{ $regex: new RegExp(queries.searchText, 'i')}});
+        }
+        if(searchTypes.indexOf('body')>=0){
+            postQueries.push({body:{$regex:new RegExp(queries.searchText, 'i')}});
+        }
+        if(postQueries.length>0) searchQuery = {$or:postQueries};
+    }
+    return searchQuery;
+}
+
 router.get('/', async (req, res) => {
     var page = Math.max(1, parseInt(req.query.page));
     var limit = Math.max(1, parseInt(req.query.limit));
@@ -19,10 +35,12 @@ router.get('/', async (req, res) => {
     page = !isNaN(page)?page:1;
     limit = !isNaN(limit)?limit:10;
 
+    var searchQuery = createSearchQuery(req.query);
+
     var skip = (page-1)*limit;
-    var count = await Post.countDocuments({});
+    var count = await Post.countDocuments(searchQuery);
     var maxPage = Math.ceil(count/limit);
-    var posts = await Post.find({})
+    var posts = await Post.find(searchQuery)
         .populate('author')
         .sort('-createdAt')
         .skip(skip)
@@ -33,7 +51,9 @@ router.get('/', async (req, res) => {
         posts:posts,
         currentPage:page,
         maxPage:maxPage,
-        limit:limit
+        limit:limit,
+        searchType:req.query.searchType,
+        searchText:req.query.searchText
     });
 });
 
@@ -51,7 +71,7 @@ router.post('/', util.isLoggedin, (req, res) => {
             req.flash('errors', util.parseError(err));
             return res.redirect('/posts/new' + res.locals.getPostQueryString());
         }
-        res.redirect('/posts' + res.locals.getPostQueryString(false, {page:1}));
+        res.redirect('/posts' + res.locals.getPostQueryString(false, {page:1, searchText:''}));
     });
 });
 
